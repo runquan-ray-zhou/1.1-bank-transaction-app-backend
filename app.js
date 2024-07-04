92160d264bdebf560ff7fadd4be70a7413732030
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-03 22:25:09 -0400

    add controller to app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require("express")
 // Configuration
 const app = express()
 
+// Controllers
+const transactionsController = require("./controllers/transactionsController")
+
+app.use("/transactions", transactionsController)
+
 // Root/Health Check Route
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to My Bank Transaction App")
