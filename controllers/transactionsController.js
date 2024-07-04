d9331cad655a62de0e9111d74deafe02308374b6
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-03 23:25:11 -0400

    add update route after testing on Postman

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -50,5 +50,17 @@ transactionsRouter.delete("/:id", (req, res) => {
     }
 })
 
+// Update Route
+transactionsRouter.put("/:id", (req, res) => {
+    const { id } = req.params
+    const transactionToUpdateIndex = transactionArray.findIndex((transaction) => transaction.id === Number(id))
+    if (transactionToUpdateIndex !== -1) {
+        transactionArray[transactionToUpdateIndex] = req.body
+        res.status(200).json(transactionArray[transactionToUpdateIndex])
+    } else {
+        res.status(404).json({error: `Transaction with id ${id} does not exist.`})
+    }
+})
+
 // Export
 module.exports = transactionsRouter;
